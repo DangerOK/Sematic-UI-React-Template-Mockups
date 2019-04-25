7a635141d98d2547b95cecf8cc3bf4924ca62060
Adam Gawish <gawishadam@gail.com>
2019-04-25 19:22:04 -0400

    Updates

---
+++ precache-manifest.a2049ac7c7ef85ce890f1adfc260fa32.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f65730da70a14da0c9b3b8bf50e30ff",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/joggers.5f65730d.jpg"
+  },
+  {
+    "revision": "2983f2adfd490c625e76",
+    "url": "/Sematic-UI-React-Template-Mockups/static/css/main.d4c53d84.chunk.css"
+  },
+  {
+    "revision": "9dd503bcd3fa43ec276b",
+    "url": "/Sematic-UI-React-Template-Mockups/static/js/runtime~main.998859d9.js"
+  },
+  {
+    "revision": "2e6b17bfba96c05ec74a",
+    "url": "/Sematic-UI-React-Template-Mockups/static/js/2.55243dcd.chunk.js"
+  },
+  {
+    "revision": "b78693e37b0d4106c8d72f23e83bbe43",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/whiteShirt.b78693e3.png"
+  },
+  {
+    "revision": "cbae0542a55e473cd3ce510e42d43841",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/blackShirt.cbae0542.svg"
+  },
+  {
+    "revision": "56bee11b16f9e316ac10f0ac7f53860d",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/greenPants.56bee11b.jpg"
+  },
+  {
+    "revision": "2983f2adfd490c625e76",
+    "url": "/Sematic-UI-React-Template-Mockups/static/js/main.3fdf1339.chunk.js"
+  },
+  {
+    "revision": "cbc5469088479fdafdcd5760565faa35",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/khakiPants.cbc54690.jpg"
+  },
+  {
+    "revision": "591abedd42e38d1a8b03809a651a763f",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/bluePants.591abedd.jpeg"
+  },
+  {
+    "revision": "c1d2c5778a2535d810f4361764b2843e",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/hat.c1d2c577.jpg"
+  },
+  {
+    "revision": "2dcebb9cf013f5939824527a2a3e79a6",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/sunglasses.2dcebb9c.png"
+  },
+  {
+    "revision": "7798e868d61bdbe84d03ccd160f5ba72",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/watch.7798e868.png"
+  },
+  {
+    "revision": "e627e217eb4e3a18b5189f03acdf7f48",
+    "url": "/Sematic-UI-React-Template-Mockups/static/media/boots.e627e217.jpg"
+  },
+  {
+    "revision": "61f69ef84d9a68442c605cda1196a481",
+    "url": "/Sematic-UI-React-Template-Mockups/index.html"
+  }
+];
